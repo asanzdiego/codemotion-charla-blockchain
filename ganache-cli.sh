0d11e1ab94210c2bf9f6d65cc49bacc3ae29b99b
Adolfo Sanz De Diego <asanzdiego@gmail.com>
2018-11-29 23:45:31 +0100

    modificado ganache-cli para no mosnemonic y las cuentas

--- ganache-cli.sh
+++ ganache-cli.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ganache-cli --gasLimit 7000001 --mnemonic "$(cat private/wallet.mnemonic)"
+echo "" > ganache-cli.log
+
+ganache-cli --gasLimit 7000001 --mnemonic "$(cat private/wallet.mnemonic)" >> ganache-cli.log &
+
+time sleep 2.0 > /dev/null
+
+tail -n 10 -f ganache-cli.log
